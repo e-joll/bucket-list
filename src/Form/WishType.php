5f8937bb76a1784fdddef21c1559ffe2f73ca176
e-joll <eleonore.jollivet@gmail.com>
2024-03-01 16:53:26 +0100

    Flash message: details.html.twig -> base.html.twig

--- src/Form/WishType.php
+++ src/Form/WishType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Wish;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -24,7 +25,11 @@ class WishType extends AbstractType
             ])
             ->add('author', TextType::class, [
                 'label' => 'Your username'
-            ]);
+            ])
+//            ->add('createdAt', DateTimeType::class, [
+//                'html5' => true,
+//                'widget' => 'single_text'
+//             ])
 //            ->add('submit', SubmitType::class)
         ;
     }
